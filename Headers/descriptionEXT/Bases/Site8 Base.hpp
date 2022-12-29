77184abf8f8a60bfe12dc2cf2d0de7c5ca7455ac
Ansible2 <afoshark@yahoo.com>
2022-12-28 21:28:41 -0700

    (#16) added site 8

--- Headers/descriptionEXT/Bases/Site8 Base.hpp
+++ Headers/descriptionEXT/Bases/Site8 Base.hpp
@@ -5,53 +5,139 @@ class Site8
         ENEMY_INFANTRY_UNIT_CLASSES
     };
 
-    class reinforceBase
-	{
-		canCall[] = {
+    class reinforceBase_1
+    {
+        canCall[] = {
+            "site8_patrol1"
+        };
+    };
 
-		};
-	};
+    class reinforceBase_2
+    {
+        canCall[] = {
+            "site8_patrol2"
+        };
+    };
 
     class infantry
     {
-        class site7_static1
+        class site8_static1
         {
             positions = "Site 8 Static Unit Markers 1";
-            numberOfUnits = 9;
-            unitsPerGroup = 3;
+            numberOfUnits = 8;
+            unitsPerGroup = 4;
             canPath = OFF;
             dynamicSim = ON;
             
-            class reinforce : reinforceBase
+            class reinforce : reinforceBase_1
+            {};
+        };
+        class site8_path1
+        {
+            positions = "Site 8 Path Unit Markers 1";
+            numberOfUnits = 7;
+            unitsPerGroup = 4;
+            canPath = ON;
+            dynamicSim = ON;
+            
+            class reinforce : reinforceBase_1
+            {};
+        };
+
+        class site8_static2 : site8_static1
+        {
+            positions = "Site 8 Static Unit Markers 2";
+            numberOfUnits = 6;
+            unitsPerGroup = 3;
+
+            class reinforce : reinforceBase_2
+            {};
+        };
+        class site8_path2 : site8_path1
+        {
+            positions = "Site 8 Path Unit Markers 2";
+            numberOfUnits = 8;
+            unitsPerGroup = 4;
+
+            class reinforce : reinforceBase_2
+            {};
+        };
+
+        class site8_static3 : site8_static1
+        {
+            positions = "Site 8 Static Unit Markers 3";
+            numberOfUnits = 9;
+            unitsPerGroup = 3;
+        };
+        class site8_path3 : site8_path1
+        {
+            positions = "Site 8 Path Unit Markers 3";
+            numberOfUnits = 8;
+            unitsPerGroup = 4;
+        };
+        
+        class site8_static4 : site8_static1
+        {
+            positions = "Site 8 Static Unit Markers 4";
+            numberOfUnits = 6;
+            unitsPerGroup = 3;
+
+            class reinforce : reinforceBase_2
+            {};
+        };
+        class site8_path4 : site8_path1
+        {
+            positions = "Site 8 Path Unit Markers 4";
+            numberOfUnits = 6;
+            unitsPerGroup = 3;
+
+            class reinforce : reinforceBase_2
             {};
         };
     };
 
     class Patrols
     {
-        class site7_patrol1
+        class site8_patrol1
         {
-            spawnPosition = "KOR_site7_patrolSpawn_1";
-            numberOfUnits = 8;
+            spawnPosition = "KOR_site8_patrolSpawn_1";
+            numberOfUnits = 6;
             behaviour = "SAFE";
             speed = "LIMITED";
             formation = "STAG COLUMN";
             dynamicSim = ON;
 
-            class RandomPatrol
+            class SpecificPatrol
             {
-                numberOfPoints = 4;
-                radius = 400;
-                waypointType = "MOVE";
+                patrolPoints = "Site 8 Patrol Markers 1";
+                random = OFF;
+                numberOfPoints = -1;
             };
+
+            class reinforce : reinforceBase_2
+            {};
+        };
+
+        class site8_patrol2 : site8_patrol1
+        {
+            spawnPosition = "KOR_site8_patrolSpawn_2";
+            numberOfUnits = 6;
+
+            class SpecificPatrol : SpecificPatrol
+            {
+                patrolPoints = "Site 8 Patrol Markers 2";
+            };
+
+            class reinforce : reinforceBase_1
+            {};
         };
     };
 
     class turrets
     {
-        class site7_turrets
+        class site8_turrets
         {
-            turrets = "Site 7 Turrets";
+            turrets = "Site 8 Turrets";
             dynamicSim = ON;
         };
     };
